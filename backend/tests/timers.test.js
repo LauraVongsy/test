c4fcbdb4a189e7dae9f871e2a3757c5c23e3a1b1
laura <vongsy.laura@gmail.com>
2023-03-01 22:26:16 +0100

    last update tests 24 hours timer

--- backend/tests/timers.test.js
+++ backend/tests/timers.test.js
@@ -1,9 +1,6 @@
-const {
-    addToQueue,
-    queue,
-    allowRandomCredits,
-} = require("../controllers/queue.controller");
+const { addToQueue, queue } = require("../controllers/queue.controller");
 let {
+    randomCredits,
     twoMinutesInterval,
     twentyFourHoursInterval,
 } = require("../controllers/queue.controller");
@@ -33,6 +30,16 @@ describe("Twenty four hours timer function", () => {
         clearInterval(twentyFourHoursInterval);
     });
     it("should generate new random credits every twenty four hours", () => {
+        // Stores the initially generated randomCredits
+        const previousRandomCredits = randomCredits;
+        // Check if twentyFourHoursInterval is running & has been called
+        expect(twentyFourHoursInterval).not.toBe(null);
         expect(setInterval).toHaveBeenCalledTimes(1);
+        // Advance time
+        jest.advanceTimersByTime(20000);
+        // Require newly generated randomCredits for comparison
+        randomCredits = require("../controllers/queue.controller");
+        const nextRandomCredits = randomCredits;
+        expect(previousRandomCredits).not.toBe(nextRandomCredits);
     });
 });
